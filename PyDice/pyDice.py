88a33e70ed9ca78a167d204fe3971c78f0004329
Taladan <taladan@gmail.com>
2017-11-19 19:58:41 -0500

    pyDice.py

---
+++ PyDice/pyDice.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+PyDice v1
+
+A program to roll dice ranging from d4 to d100.
+
+Author: Taladan
+Last Edited: November 2017
+"""
+
+import random, sys
+from PyQt5.QtWidgets import (QWidget, QLabel, QComboBox, QPushButton,
+                             QHBoxLayout, QVBoxLayout, QApplication)
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QCoreApplication
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def set_dice_total(self, roll_table):
+        roll_table['roll_total'] = self.num_of_dice.currentText()
+        return roll_table
+
+    def set_die_type(self, roll_table):
+        roll_table['die'] = self.die_type.currentText()
+        return roll_table
+
+    def roll(self, roll_table):
+        die = int(roll_table['die'][1:])
+        tot = int(roll_table['roll_total'])
+        choice = 0
+        for i in range(tot):
+            choice = choice + random.randrange(1, die + 1)
+        self.disp.setText(str(choice))
+
+    def initUI(self):
+
+        roll_table = {'die': 'd4', 'roll_total': '1'}
+
+        self.dice = ['d4', 'd6', 'd8', 'd10', 'd12', 'd20', 'd100']
+        self.dice_num_lbl = QLabel("# of Die to roll", self)
+        self.die_type_lbl = QLabel("Type of Die to roll", self)
+
+        # Create comboBoxes
+        self.num_of_dice = QComboBox(self)
+        self.num_of_dice.activated.connect(lambda: self.set_dice_total(roll_table))
+        self.die_type = QComboBox(self)
+        self.die_type.activated.connect(lambda: self.set_die_type(roll_table))
+
+        # Build contents of comboBoxes
+        for num in range(1, 51):
+            self.num_of_dice.addItem(str(num))
+
+        for num in self.dice:
+            self.die_type.addItem(str(num))
+
+        # Create Output Display
+        self.disp = QLabel(self)
+        self.disp.setFont(QFont('Sans Serif', 20))
+
+        # Assign buttons
+        rollBut = QPushButton('Roll')
+        rollBut.clicked.connect(lambda: self.roll(roll_table))
+        quitBut = QPushButton('Quit')
+        quitBut.clicked.connect(QCoreApplication.instance().quit)
+
+        # Layout Display
+        hbox_disp = QHBoxLayout()
+        hbox_disp.addStretch(1)
+        hbox_disp.addWidget(self.disp)
+        hbox_disp.addStretch(1)
+
+        # Layout labels
+        hbox_lbls = QHBoxLayout()
+        hbox_lbls.addStretch(1)
+        hbox_lbls.addWidget(self.dice_num_lbl)
+        hbox_lbls.addStretch(1)
+        hbox_lbls.addWidget(self.die_type_lbl)
+        hbox_lbls.addStretch(1)
+
+        # Layout comboBoxes
+        hbox_die = QHBoxLayout()
+        hbox_die.addStretch(1)
+        hbox_die.addWidget(self.num_of_dice)
+        hbox_die.addStretch(1)
+        hbox_die.addWidget(self.die_type)
+        hbox_die.addStretch(1)
+
+        # Layout Buttons
+        hbox_but = QHBoxLayout()
+        hbox_but.addStretch(1)
+        hbox_but.addWidget(rollBut)
+        hbox_but.addWidget(quitBut)
+        hbox_but.addStretch(1)
+
+        # Package layouts into full display
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox_disp)
+        vbox.addStretch(1)
+        vbox.addLayout(hbox_lbls)
+        vbox.addStretch(1)
+        vbox.addStretch(1)
+        vbox.addLayout(hbox_die)
+        vbox.addStretch(1)
+        vbox.addLayout(hbox_but)
+
+        # Set layout to vbox
+        self.setLayout(vbox)
+
+        # Set Window Geometry and display it
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle('PyDice Roller')
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
