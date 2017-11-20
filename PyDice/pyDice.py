bac2a1a2eaecbc42b04152785db2c31628a3be08
Taladan <taladan@gmail.com>
2017-11-20 08:12:00 -0500

    PyDice update v1.1 - added option to remove lowest die from roll, re-organized and commented code for readability

--- PyDice/pyDice.py
+++ PyDice/pyDice.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-PyDice v1
+PyDice v1.1
 
 A program to roll dice ranging from d4 to d100.
 
@@ -10,41 +10,63 @@ Author: Taladan
 Last Edited: November 2017
 """
 
-import random, sys
-from PyQt5.QtWidgets import (QWidget, QLabel, QComboBox, QPushButton,
-                             QHBoxLayout, QVBoxLayout, QApplication)
-from PyQt5.QtGui import QFont
 from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QHBoxLayout,
+                             QLabel, QPushButton, QVBoxLayout, QWidget)
+
+import random
+import sys
 
 
-class Example(QWidget):
+class PyDice(QWidget):
 
     def __init__(self):
         super().__init__()
 
         self.initUI()
 
+    # Set the number of dice to roll
     def set_dice_total(self, roll_table):
         roll_table['roll_total'] = self.num_of_dice.currentText()
         return roll_table
 
+    # Set the type of dice to roll
     def set_die_type(self, roll_table):
         roll_table['die'] = self.die_type.currentText()
         return roll_table
 
-    def roll(self, roll_table):
+    # Roll & update label
+    def roll(self, roll_table, ischecked):
+
         die = int(roll_table['die'][1:])
         tot = int(roll_table['roll_total'])
-        choice = 0
+
+        choice_lst = []
+
         for i in range(tot):
-            choice = choice + random.randrange(1, die + 1)
+            choice_lst.append(random.randrange(1, die + 1))
+
+        if ischecked and tot > 1:
+            choice_lst.remove(min(choice_lst))
+
+        choice = sum(choice_lst)
         self.disp.setText(str(choice))
 
+    # Build the display
     def initUI(self):
 
+        # Dictionary that stores type of dice and number to roll
         roll_table = {'die': 'd4', 'roll_total': '1'}
 
+        # List of dice type
         self.dice = ['d4', 'd6', 'd8', 'd10', 'd12', 'd20', 'd100']
+
+        # Create Output Display
+        self.disp = QLabel(self)
+        self.disp.setFont(QFont('Sans Serif', 20))
+
+        # Create ComboBox Labels
         self.dice_num_lbl = QLabel("# of Die to roll", self)
         self.die_type_lbl = QLabel("Type of Die to roll", self)
 
@@ -61,13 +83,13 @@ class Example(QWidget):
         for num in self.dice:
             self.die_type.addItem(str(num))
 
-        # Create Output Display
-        self.disp = QLabel(self)
-        self.disp.setFont(QFont('Sans Serif', 20))
+        # Build checkbox
+        self.rm_lowest_cb = QCheckBox('Remove Lowest')
 
         # Assign buttons
         rollBut = QPushButton('Roll')
-        rollBut.clicked.connect(lambda: self.roll(roll_table))
+        rollBut.clicked.connect(lambda: self.roll(roll_table,
+                                                  self.rm_lowest_cb.isChecked()))
         quitBut = QPushButton('Quit')
         quitBut.clicked.connect(QCoreApplication.instance().quit)
 
@@ -110,6 +132,8 @@ class Example(QWidget):
         vbox.addStretch(1)
         vbox.addLayout(hbox_die)
         vbox.addStretch(1)
+        vbox.addWidget(self.rm_lowest_cb)
+        vbox.addStretch(1)
         vbox.addLayout(hbox_but)
 
         # Set layout to vbox
@@ -124,5 +148,5 @@ class Example(QWidget):
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-    ex = Example()
+    pd = PyDice()
     sys.exit(app.exec_())
